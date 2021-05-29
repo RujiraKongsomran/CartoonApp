56c97934b54df0c45ca058ff86d0901741b8f84f
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-05-29 15:42:02 +0700

    Call Service using Volley finish

--- app/src/main/java/com/rujirakongsomran/cartoonapp/activity/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/cartoonapp/activity/MainActivity.java
@@ -1,16 +1,91 @@
 package com.rujirakongsomran.cartoonapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.rujirakongsomran.cartoonapp.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.rujirakongsomran.cartoonapp.adapter.RecyclerViewAdapter;
+import com.rujirakongsomran.cartoonapp.databinding.ActivityMainBinding;
+import com.rujirakongsomran.cartoonapp.model.Anime;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private final String JSON_URL = "https://gist.githubusercontent.com/aws1994/f583d54e5af8e56173492d3f60dd5ebf/raw/c7796ba51d5a0d37fc756cf0fd14e54434c547bc/anime.json";
+    private JsonArrayRequest request;
+    private RequestQueue requestQueue;
+    private List<Anime> lstData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        lstData = new ArrayList<>();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        jsonRequest();
+    }
+
+    private void jsonRequest() {
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+
+                        jsonObject = response.getJSONObject(i);
+                        Anime anime = new Anime();
+                        anime.setName(jsonObject.getString("name"));
+                        anime.setDescription(jsonObject.getString("description"));
+                        anime.setRating(jsonObject.getString("Rating"));
+                        anime.setCategories(jsonObject.getString("categorie"));
+                        anime.setEpisode(jsonObject.getInt("episode"));
+                        anime.setStudio(jsonObject.getString("studio"));
+                        anime.setImg(jsonObject.getString("img"));
+                        lstData.add(anime);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                setupRecyclerView(lstData);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(request);
+    }
+
+    private void setupRecyclerView(List<Anime> lstData) {
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, lstData);
+        binding.recyclerview.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerview.setAdapter(adapter);
     }
 }
