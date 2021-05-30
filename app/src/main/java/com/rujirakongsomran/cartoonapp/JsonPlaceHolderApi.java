b6786e590d03f61d9cb323b25b47ea38379614ed
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-05-30 12:50:11 +0700

    Call Service using Retrofit2 : Create Interface

---
+++ app/src/main/java/com/rujirakongsomran/cartoonapp/JsonPlaceHolderApi.java
@@ -0,0 +1,11 @@
+package com.rujirakongsomran.cartoonapp;
+
+import com.rujirakongsomran.cartoonapp.model.Anime;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("/aws1994/f583d54e5af8e56173492d3f60dd5ebf/raw/c7796ba51d5a0d37fc756cf0fd14e54434c547bc/anime.json")
+    Call<Anime> getListAnime();
+}
